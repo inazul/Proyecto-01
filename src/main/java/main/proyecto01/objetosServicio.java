4fbcfcd56a546785669548a3c94f2a1b795334f0
ElMataDev <DiegoGuerra_M@Outlook.com>
2025-03-08 20:54:23 -0700

    Proyecto 01
    Se creo la base para el proyecto...

---
+++ src/main/java/main/proyecto01/objetosServicio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package main.proyecto01;
+
+/**
+ *
+ * @author DG
+ */
+public class objetosServicio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
